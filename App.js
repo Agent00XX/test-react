35342f83be95a709bfa5a29548472b99b97acb3d
Agent00XX <33354076+Agent00XX@users.noreply.github.com>
2021-07-26 12:02:42 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Dashboard from './Dashboard/Dashboard.js';
+import './Dashboard/Dashboard.css';
+
+function App() {
+  return (
+      <Dashboard />
+  );
+}
+
+export default App;
